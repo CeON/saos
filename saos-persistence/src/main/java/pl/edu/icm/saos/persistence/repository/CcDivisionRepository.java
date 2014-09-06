cad5398a327282cf0e4ee7c725bd482d4e5371f1
pawelczak <L.Pawelczak@icm.edu.pl>
2014-09-06 21:54:04 +0200

    Added to ccDivisionRepositiory method to retrieve all divisions. Fixed #64

--- saos-persistence/src/main/java/pl/edu/icm/saos/persistence/repository/CcDivisionRepository.java
+++ saos-persistence/src/main/java/pl/edu/icm/saos/persistence/repository/CcDivisionRepository.java
@@ -1,14 +1,17 @@
 package pl.edu.icm.saos.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pl.edu.icm.saos.persistence.model.CommonCourtDivision;
 
 /**
- * @author Łukasz Dumiszewski
+ * @author Ĺ�ukasz Dumiszewski
  */
 public interface CcDivisionRepository extends JpaRepository<CommonCourtDivision, Integer> { 
 
     CommonCourtDivision findOneByCourtIdAndCode(int courtId, String code);
     
+    List<CommonCourtDivision> findAllByCourtId(int courtId);
 }
--- saos-persistence/src/test/java/pl/edu/icm/saos/persistence/repository/CcDivisionRepositoryTest.java
+++ saos-persistence/src/test/java/pl/edu/icm/saos/persistence/repository/CcDivisionRepositoryTest.java
@@ -1,11 +1,22 @@
 package pl.edu.icm.saos.persistence.repository;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.google.common.collect.Lists;
 
 import pl.edu.icm.saos.common.testcommon.category.SlowTest;
 import pl.edu.icm.saos.persistence.PersistenceTestSupport;
@@ -13,7 +24,7 @@ import pl.edu.icm.saos.persistence.model.CommonCourt;
 import pl.edu.icm.saos.persistence.model.CommonCourtDivision;
 
 /**
- * @author Łukasz Dumiszewski
+ * @author Ĺ�ukasz Dumiszewski
  */
 @Category(SlowTest.class)
 public class CcDivisionRepositoryTest extends PersistenceTestSupport {
@@ -24,21 +35,75 @@ public class CcDivisionRepositoryTest extends PersistenceTestSupport {
     @Autowired
     private CommonCourtRepository commonCourtRepository;
     
+    private CommonCourt court = new CommonCourt();
+    
+    private CommonCourt courtEmpty = new CommonCourt();
+    
+    private List<CommonCourtDivision> courtDivisions = Lists.newArrayList();
+    
+    private Map<String, CommonCourt> commonCourtMap = new HashMap<String, CommonCourt>(); 
+    
+    @Before
+    public void before() {
+    	super.before();
+    	initializeTests();
+    }
+    
     @Test
     public void findByCourtIdAndCode_NotFound() {
-        assertNull(ccDivisionRepository.findOneByCourtIdAndCode(23, "12"));
+    	String code = "25";
+    	
+        assertNull(ccDivisionRepository.findOneByCourtIdAndCode(commonCourtMap.get(code).getId(), code));
     }
 
     @Test
     public void findByCourtIdAndCode_Found() {
-        CommonCourt court = new CommonCourt();
-        commonCourtRepository.save(court);
+        String code = "23";
+        CommonCourt testCourt = commonCourtMap.get(code);
         
-        CommonCourtDivision ccDivision = new CommonCourtDivision();
-        ccDivision.setCourt(court);
-        ccDivision.setCode("12");
-        ccDivisionRepository.save(ccDivision);
-        assertNotNull(ccDivisionRepository.findOneByCourtIdAndCode(ccDivision.getCourt().getId(), ccDivision.getCode()));
+        CommonCourtDivision testDivision = ccDivisionRepository.findOneByCourtIdAndCode(testCourt.getId(), code); 
+        
+        assertNotNull(testDivision);
+    	assertEquals(courtDivisions.get(0), testDivision);
     }
 
+    @Test
+    public void findAllByCourtId_NotFound() {
+    	List<CommonCourtDivision> emptyDivisions = ccDivisionRepository.findAllByCourtId(commonCourtMap.get("25").getId());
+    	
+	     assertNotNull(emptyDivisions);
+	     assertTrue(emptyDivisions.isEmpty());
+    }
+    
+    @Test
+    public void findAllByCourtId_Found() {
+    	List<CommonCourtDivision> testDivisions = ccDivisionRepository.findAllByCourtId(commonCourtMap.get("23").getId()); 
+
+    	assertNotNull(testDivisions);
+    	assertEquals(courtDivisions.size(), testDivisions.size());
+    	assertEquals(courtDivisions, testDivisions);
+    }
+    
+    
+    private void initializeTests() {
+    	commonCourtMap.put("23", court);
+    	commonCourtRepository.save(court);
+        
+        courtDivisions.add(createAndSaveCCDivision("23", court));
+        courtDivisions.add(createAndSaveCCDivision("24", court));
+        
+        commonCourtMap.put("25", courtEmpty);
+    	commonCourtRepository.save(courtEmpty);
+    }
+    
+    private CommonCourtDivision createAndSaveCCDivision(String hashCode, CommonCourt court) {
+    	CommonCourtDivision ccDivision = new CommonCourtDivision();
+    	ccDivision.setCourt(court);
+    	ccDivision.setCode(hashCode);
+        ccDivisionRepository.save(ccDivision);
+        return ccDivision;
+    }
+    
+
+    
 }
