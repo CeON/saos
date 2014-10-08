a1030ae9c999e15348c37a4c2e6df0c7d45e18b0
Łukasz Dumiszewski <l.dumiszewski@icm.edu.pl>
2014-10-08 09:06:34 +0200

    Fixed #102: Processing of imported supreme court judgments - read part

--- saos-persistence/src/main/java/pl/edu/icm/saos/persistence/repository/SimpleRawSourceScJudgmentRepository.java
+++ saos-persistence/src/main/java/pl/edu/icm/saos/persistence/repository/SimpleRawSourceScJudgmentRepository.java
@@ -1,6 +1,9 @@
 package pl.edu.icm.saos.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import pl.edu.icm.saos.persistence.model.importer.SimpleRawSourceScJudgment;
 
@@ -9,5 +12,12 @@ import pl.edu.icm.saos.persistence.model.importer.SimpleRawSourceScJudgment;
  */
 
 public interface SimpleRawSourceScJudgmentRepository extends JpaRepository<SimpleRawSourceScJudgment, Integer> {
+
+    
+    /**
+     * Finds all {@link SimpleRawSourceScJudgment}s that are not marked as processed ({@link SimpleRawSourceScJudgment#isProcessed()})
+     */
+    @Query("select rJudgment.id from SimpleRawSourceScJudgment rJudgment where rJudgment.processed=false order by id")
+    List<Integer> findAllNotProcessedIds();
 
 }
