b4c1480c62d0445b52d56fc1a2843c1e59fa4be9
Łukasz Dumiszewski <l.dumiszewski@icm.edu.pl>
2014-10-17 11:55:49 +0200

    Fixed #192: Deny access for robots to SAOS app on test server

--- saos-webapp/src/main/java/pl/edu/icm/saos/webapp/WebappConfiguration.java
+++ saos-webapp/src/main/java/pl/edu/icm/saos/webapp/WebappConfiguration.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.data.web.config.SpringDataWebConfiguration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
@@ -38,6 +38,7 @@ public class WebappConfiguration extends SpringDataWebConfiguration {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("/WEB-INF/static/").setCachePeriod(3600*24*7);
+        registry.addResourceHandler("/robots.txt").addResourceLocations("/WEB-INF/").setCachePeriod(0);
     }
 
 
