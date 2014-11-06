9a6ccd621a50b9addbe3e0b222c10415758428bf
Jan Pavtel <janpavtel@gmail.com>
2014-11-06 10:29:18 +0100

    added Dump chambers controller

--- saos-restful-web-api/src/main/java/pl/edu/icm/saos/api/dump/supreme/court/chamber/DumpSupremeCourtChambersController.java
+++ saos-restful-web-api/src/main/java/pl/edu/icm/saos/api/dump/supreme/court/chamber/DumpSupremeCourtChambersController.java
@@ -1,4 +1,4 @@
-package pl.edu.icm.saos.api.dump.court;
+package pl.edu.icm.saos.api.dump.supreme.court.chamber;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -13,11 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import pl.edu.icm.saos.api.dump.court.views.DumpCourtsView;
 import pl.edu.icm.saos.api.search.parameters.Pagination;
 import pl.edu.icm.saos.api.search.parameters.ParametersExtractor;
-import pl.edu.icm.saos.api.services.exceptions.ControllersEntityExceptionHandler;
 import pl.edu.icm.saos.api.services.exceptions.WrongRequestParameterException;
 import pl.edu.icm.saos.persistence.model.CommonCourt;
+import pl.edu.icm.saos.persistence.model.SupremeCourtChamber;
 import pl.edu.icm.saos.persistence.search.DatabaseSearchService;
 import pl.edu.icm.saos.persistence.search.dto.CommonCourtSearchFilter;
+import pl.edu.icm.saos.persistence.search.dto.SupremeCourtChamberSearchFilter;
 import pl.edu.icm.saos.persistence.search.result.SearchResult;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -28,17 +29,14 @@ import static pl.edu.icm.saos.api.ApiConstants.PAGE_SIZE;
  * @author pavtel
  */
 @Controller
-@RequestMapping("/api/dump/courts")
-public class DumpCourtsController extends ControllersEntityExceptionHandler {
+@RequestMapping("/api/dump/supremeCourtChambers")
+public class DumpSupremeCourtChambersController {
 
     @Autowired
     private ParametersExtractor parametersExtractor;
 
     @Autowired
     private DatabaseSearchService databaseSearchService;
-
-    @Autowired
-    private DumpCourtsListSuccessRepresentationBuilder dumpCourtsListSuccessRepresentationBuilder;
 
 
     @RequestMapping(value = "", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -50,36 +48,19 @@ public class DumpCourtsController extends ControllersEntityExceptionHandler {
 
         Pagination pagination = parametersExtractor.extractAndValidatePagination(pageSize, pageNumber);
 
-
-
-        CommonCourtSearchFilter searchFilter = CommonCourtSearchFilter.builder()
+        SupremeCourtChamberSearchFilter searchFilter = SupremeCourtChamberSearchFilter.builder()
                 .limit(pagination.getPageSize())
                 .offset(pagination.getOffset())
                 .filter();
 
-        SearchResult<CommonCourt> searchResult = databaseSearchService.search(searchFilter);
+        SearchResult<SupremeCourtChamber> searchResult = databaseSearchService.search(searchFilter);
 
 
-        DumpCourtsView representation = dumpCourtsListSuccessRepresentationBuilder
-                .build(searchResult, pagination, linkTo(DumpCourtsController.class).toUriComponentsBuilder());
+        Object representation = dumpCourtsListSuccessRepresentationBuilder
+                .build(searchResult, pagination, linkTo(DumpSupremeCourtChambersController.class).toUriComponentsBuilder());
 
         HttpHeaders httpHeaders = new HttpHeaders();
 
         return new ResponseEntity<>(representation, httpHeaders, HttpStatus.OK);
-    }
-
-
-    //*** setters ***
-
-    public void setParametersExtractor(ParametersExtractor parametersExtractor) {
-        this.parametersExtractor = parametersExtractor;
-    }
-
-    public void setDatabaseSearchService(DatabaseSearchService databaseSearchService) {
-        this.databaseSearchService = databaseSearchService;
-    }
-
-    public void setDumpCourtsListSuccessRepresentationBuilder(DumpCourtsListSuccessRepresentationBuilder dumpCourtsListSuccessRepresentationBuilder) {
-        this.dumpCourtsListSuccessRepresentationBuilder = dumpCourtsListSuccessRepresentationBuilder;
     }
 }
