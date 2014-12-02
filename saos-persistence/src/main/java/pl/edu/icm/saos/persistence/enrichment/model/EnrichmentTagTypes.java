b3913125ba1845aeee98d9882d51ada846b432ea
Łukasz Dumiszewski <l.dumiszewski@icm.edu.pl>
2014-12-02 10:57:13 +0100

    Fixed #316: EnrichmentTag class impl

---
+++ saos-persistence/src/main/java/pl/edu/icm/saos/persistence/enrichment/model/EnrichmentTagTypes.java
@@ -0,0 +1,29 @@
+package pl.edu.icm.saos.persistence.enrichment.model;
+
+/**
+ * String enrichment tag types that are recognizable and processed by SAOS.  <br/>
+ * 
+ * @author Łukasz Dumiszewski
+ */
+
+public final class EnrichmentTagTypes {
+
+    
+    private EnrichmentTagTypes() {
+        throw new IllegalStateException("may not be instantiated");
+    }
+    
+    
+    
+    /** ids of judgments whose text contents are 'similar' to a given judgment, similarity is specified by
+     * algorithms in SAOS Enricher */
+    public static final String SIMILAR_JUDGMENTS = "SIMILAR_JUDGMENTS";
+    
+    
+    /** case numbers referenced in a text content of a judgment */
+    public static final String REFERENCED_CASE_NUMBERS = "REFERENCED_CASE_NUMBERS";
+    
+    
+    /** referenced regulations referenced in a text content of a judgment and not provided by a judgment source system */
+    public static final String REFERENCED_REGULATIONS = "REFERENCED_REGULATIONS";
+}
