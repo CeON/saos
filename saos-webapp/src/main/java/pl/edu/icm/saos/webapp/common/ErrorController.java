792e854bed3ed2a8719a470b956ebebeca300c63
Łukasz Pawełczak <l.pawelczak@icm.edu.pl>
2014-12-04 18:04:37 +0100

    Closes #274. Added logical sections to web classes.

--- saos-webapp/src/main/java/pl/edu/icm/saos/webapp/common/ErrorController.java
+++ saos-webapp/src/main/java/pl/edu/icm/saos/webapp/common/ErrorController.java
@@ -9,16 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/errors")
 public class ErrorController {
+	
+	
+	//------------------------ LOGIC --------------------------
+	
+	@RequestMapping("/404")
+	public String error404() {
+		return "error404";
+	}
+	
+	@RequestMapping("/500")
+	public String error500() {
+		return "error500";
+	}
 
-
-    @RequestMapping("/404")
-    public String error404() {
-        return "error404";
-    }
-    
-    @RequestMapping("/500")
-    public String error500() {
-        return "error500";
-    }
-    
 }
