5446bbb70449bf55f7f14320e563659da1cf8c50
Krzysztof Mądry <madryk@icm.edu.pl>
2014-12-15 10:28:41 +0100

    Closes #346. constitutional tribunal judgment model

---
+++ saos-persistence/src/main/java/pl/edu/icm/saos/persistence/model/ConstitutionalTribunalJudgment.java
@@ -0,0 +1,69 @@
+package pl.edu.icm.saos.persistence.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+/**
+ * @author madryk
+ */
+@Entity
+public class ConstitutionalTribunalJudgment extends Judgment {
+
+    private List<ConstitutionalTribunalJudgmentDissentingOpinion> dissentingOpinions = Lists.newArrayList();
+
+    
+    //------------------------ GETTERS --------------------------
+    
+    @Transient
+    @Override
+    public CourtType getCourtType() {
+        return CourtType.CONSTITUTIONAL_TRIBUNAL;
+    }
+
+    @OneToMany(mappedBy="judgment", orphanRemoval=true, cascade=CascadeType.ALL)
+    private List<ConstitutionalTribunalJudgmentDissentingOpinion> getDissentingOpinions_() {
+        return dissentingOpinions;
+    }
+    
+    @Transient
+    public List<ConstitutionalTribunalJudgmentDissentingOpinion> getDissentingOpinions() {
+        return ImmutableList.copyOf(getDissentingOpinions_());
+    }
+    
+    
+    //------------------------ LOGIC --------------------------
+    
+    public void addDissentingOpinion(ConstitutionalTribunalJudgmentDissentingOpinion dissentingOpinion) {
+        Preconditions.checkArgument(!containsDissentingOpinion(dissentingOpinion));
+        
+        this.dissentingOpinions.add(dissentingOpinion);
+    }
+
+    public boolean containsDissentingOpinion(ConstitutionalTribunalJudgmentDissentingOpinion dissentingOpinion) {
+        return this.dissentingOpinions.contains(dissentingOpinion);
+    }
+    
+    public void removeDissentingOpinion(ConstitutionalTribunalJudgmentDissentingOpinion dissentingOpinion) {
+        this.dissentingOpinions.remove(dissentingOpinion);
+    }
+
+    
+    //------------------------ SETTERS --------------------------
+    
+    @SuppressWarnings("unused") // for hibernate
+    private void setDissentingOpinions_(
+            List<ConstitutionalTribunalJudgmentDissentingOpinion> dissentingOpinions) {
+        this.dissentingOpinions = dissentingOpinions;
+    }
+
+    
+    
+}
