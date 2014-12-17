f32952f8bf00956c25dacdc583265cd5192e21d9
Jan Pavtel <janpavtel@gmail.com>
2014-12-17 17:14:27 +0100

    one

---
+++ saos-restful-web-api/src/main/java/pl/edu/icm/saos/api/single/judgment/data/representation/ConstitutionalTribunalJudgmentData.java
@@ -0,0 +1,11 @@
+package pl.edu.icm.saos.api.single.judgment.data.representation;
+
+/**
+ * Represents {@link pl.edu.icm.saos.persistence.model.ConstitutionalTribunalJudgment ConstitutionalTribunalJudgment} fields.
+ * @author pavtel
+ */
+public class ConstitutionalTribunalJudgmentData extends JudgmentData{
+    private static final long serialVersionUID = 3202843536990337571L;
+
+
+}
