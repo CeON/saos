e81e708e8f3b43bf67af2a17ddc6778248961037
Krzysztof Mądry <madryk@icm.edu.pl>
2015-01-07 12:58:26 +0100

    Closes #398. Constitutional Tribunal Judgments import process configuration

--- saos-import/src/main/java/pl/edu/icm/saos/importer/notapi/constitutionaltribunal/ConstitutionalTribunalImportConfiguration.java
+++ saos-import/src/main/java/pl/edu/icm/saos/importer/notapi/constitutionaltribunal/ConstitutionalTribunalImportConfiguration.java
@@ -7,11 +7,20 @@ import org.springframework.context.annotation.Configuration;
 
 import pl.edu.icm.saos.common.json.JsonStringParser;
 import pl.edu.icm.saos.common.validation.CommonValidator;
-import pl.edu.icm.saos.importer.notapi.common.StringItemImportDownloadProcessor;
+import pl.edu.icm.saos.importer.common.converter.JudgmentConverter;
+import pl.edu.icm.saos.importer.common.converter.JudgmentConverterImpl;
+import pl.edu.icm.saos.importer.common.overwriter.DelegatingJudgmentOverwriter;
+import pl.edu.icm.saos.importer.common.overwriter.JudgmentOverwriter;
 import pl.edu.icm.saos.importer.notapi.common.JsonImportDownloadReader;
+import pl.edu.icm.saos.importer.notapi.common.JsonJudgmentImportProcessProcessor;
+import pl.edu.icm.saos.importer.notapi.common.JudgmentImportProcessReader;
 import pl.edu.icm.saos.importer.notapi.common.NotApiImportDownloadStepExecutionListener;
+import pl.edu.icm.saos.importer.notapi.common.StringItemImportDownloadProcessor;
 import pl.edu.icm.saos.importer.notapi.constitutionaltribunal.judgment.download.RawSourceCtJudgmentParser;
 import pl.edu.icm.saos.importer.notapi.constitutionaltribunal.judgment.json.SourceCtJudgment;
+import pl.edu.icm.saos.importer.notapi.constitutionaltribunal.judgment.process.CtSpecificJudgmentOverwriter;
+import pl.edu.icm.saos.importer.notapi.constitutionaltribunal.judgment.process.SourceCtJudgmentExtractor;
+import pl.edu.icm.saos.persistence.model.ConstitutionalTribunalJudgment;
 import pl.edu.icm.saos.persistence.model.importer.notapi.RawSourceCtJudgment;
 
 import com.fasterxml.jackson.databind.MappingJsonFactory;
@@ -30,6 +39,12 @@ public class ConstitutionalTribunalImportConfiguration {
     
     @Autowired
     private RawSourceCtJudgmentParser rawSourceCtJudgmentParser;
+    
+    @Autowired
+    private SourceCtJudgmentExtractor sourceCtJudgmentExtractor;
+    
+    @Autowired 
+    private CtSpecificJudgmentOverwriter ctSpecificJudgmentOverwriter;
     
     
     //------------------------ BEANS --------------------------
@@ -64,5 +79,38 @@ public class ConstitutionalTribunalImportConfiguration {
         stepExecutionListener.setRawJudgmentClass(RawSourceCtJudgment.class);
         
         return stepExecutionListener;
+    }
+    
+    @Bean
+    public JudgmentConverter<ConstitutionalTribunalJudgment, SourceCtJudgment> sourceCtJudgmentConverter() {
+        JudgmentConverterImpl<ConstitutionalTribunalJudgment, SourceCtJudgment> judgmentConverter = new JudgmentConverterImpl<>();
+        judgmentConverter.setJudgmentDataExtractor(sourceCtJudgmentExtractor);
+        return judgmentConverter;
+    }
+    
+    
+    @Bean
+    public JudgmentOverwriter<ConstitutionalTribunalJudgment> ctJudgmentOverwriter() {
+        DelegatingJudgmentOverwriter<ConstitutionalTribunalJudgment> ctJudgmentOverwriter = new DelegatingJudgmentOverwriter<>();
+        ctJudgmentOverwriter.setSpecificJudgmentOverwriter(ctSpecificJudgmentOverwriter);
+        return ctJudgmentOverwriter;
+    }
+
+
+
+    @Bean
+    public JudgmentImportProcessReader<RawSourceCtJudgment> ctjImportProcessReader() {
+        JudgmentImportProcessReader<RawSourceCtJudgment> importProcessReader = new JudgmentImportProcessReader<>(RawSourceCtJudgment.class);
+        return importProcessReader;
+    }
+    
+    @Bean
+    public JsonJudgmentImportProcessProcessor<SourceCtJudgment, ConstitutionalTribunalJudgment> ctjImportProcessProcessor() {
+        JsonJudgmentImportProcessProcessor<SourceCtJudgment, ConstitutionalTribunalJudgment> ctjImportProcessProcessor = new JsonJudgmentImportProcessProcessor<>(ConstitutionalTribunalJudgment.class);
+        ctjImportProcessProcessor.setSourceJudgmentParser(sourceCtJudgmentParser());
+        ctjImportProcessProcessor.setSourceJudgmentConverter(sourceCtJudgmentConverter());
+        ctjImportProcessProcessor.setJudgmentOverwriter(ctJudgmentOverwriter());
+        
+        return ctjImportProcessProcessor;
     }
 }
