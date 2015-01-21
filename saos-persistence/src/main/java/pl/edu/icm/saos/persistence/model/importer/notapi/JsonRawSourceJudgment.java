bfb7c76cc8d6da37aa9e47403c7b682a96e8c782
Łukasz Dumiszewski <l.dumiszewski@icm.edu.pl>
2015-01-21 08:41:19 +0100

    Closes #439: Unblock nac and ct court types in search + hashCode/equals for JsonRawSourceJudgment

--- saos-persistence/src/main/java/pl/edu/icm/saos/persistence/model/importer/notapi/JsonRawSourceJudgment.java
+++ saos-persistence/src/main/java/pl/edu/icm/saos/persistence/model/importer/notapi/JsonRawSourceJudgment.java
@@ -36,4 +36,47 @@ public class JsonRawSourceJudgment extends RawSourceJudgment {
     public void setJsonContent(String jsonContent) {
         this.jsonContent = jsonContent;
     }
+    
+    //------------------------ HashCode & Equals --------------------------
+    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((getJsonContent() == null) ? 0 : getJsonContent().hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        JsonRawSourceJudgment other = (JsonRawSourceJudgment) obj;
+        if (getJsonContent() == null) {
+            if (other.getJsonContent() != null)
+                return false;
+        } else if (!getJsonContent().equals(other.getJsonContent()))
+            return false;
+        return true;
+    }
+
+
+   
+
+    //------------------------ toString --------------------------
+   
+    @Override
+    public String toString() {
+        return "JsonRawSourceJudgment [jsonContent=" + getJsonContent()
+                + ", id=" + id + ", isProcessed()=" + isProcessed()
+                + ", getProcessingDate()=" + getProcessingDate() + "]";
+    }
+
 }
