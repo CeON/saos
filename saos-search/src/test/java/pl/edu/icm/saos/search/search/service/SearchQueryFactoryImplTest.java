55ecd071a767b2fa080a1a5606850ad08476915f
Krzysztof Mądry <madryk@icm.edu.pl>
2015-01-29 09:48:53 +0100

    #454. additional test, changes in schema

--- saos-search/src/test/java/pl/edu/icm/saos/search/search/service/SearchQueryFactoryImplTest.java
+++ saos-search/src/test/java/pl/edu/icm/saos/search/search/service/SearchQueryFactoryImplTest.java
@@ -38,38 +38,56 @@ public class SearchQueryFactoryImplTest {
     
     @Test
     public void createQuery_TRANSFORM_CRITERIA() {
+        // given
         when(criteriaTransformer.transformCriteria(any())).thenReturn("+keyword:word");
         
+        // execute
         SolrQuery solrQuery = queryFactory.createQuery(new JudgmentCriteria(), null);
         
+        // assert
         assertEquals("+keyword:word", solrQuery.getQuery());
     }
     
     @Test
     public void createQuery_PAGING() {
+        // given
         JudgmentCriteria criteria = new JudgmentCriteria();
         Paging paging = new Paging(3, 15);
+        
+        when(criteriaTransformer.transformCriteria(any())).thenReturn("*:*");
 
+        // execute
         SolrQuery solrQuery = queryFactory.createQuery(criteria, paging);
 
+        // assert
         assertEquals(Integer.valueOf(45), solrQuery.getStart());
         assertEquals(Integer.valueOf(15), solrQuery.getRows());
     }
 
     @Test
     public void createQuery_SORTING() {
+        // given
         JudgmentCriteria criteria = new JudgmentCriteria();
         Paging paging = new Paging(0, 10, Sorting.relevanceSorting());
+        
+        when(criteriaTransformer.transformCriteria(any())).thenReturn("*:*");
 
+        // execute
         SolrQuery solrQuery = queryFactory.createQuery(criteria, paging);
 
+        // assert
         assertEquals("score desc", solrQuery.getSortField());
     }
 
     @Test
     public void createQuery_HIGHLIGHTING() {
+        // given
+        when(criteriaTransformer.transformCriteria(any())).thenReturn("*:*");
+        
+        // execute
         SolrQuery solrQuery = queryFactory.createQuery(new JudgmentCriteria(), null);
 
+        // assert
         assertTrue(solrQuery.getHighlight());
         assertEquals("<highlight>", solrQuery.getHighlightSimplePre());
         assertEquals("</highlight>", solrQuery.getHighlightSimplePost());
@@ -85,6 +103,18 @@ public class SearchQueryFactoryImplTest {
         assertEquals("5", solrQuery.getFieldParam("secondContent", HighlightParams.SNIPPETS));
     }
     
+    @Test
+    public void createQuery_HIGHLIGHTING_FROM_FIELD() {
+        // given
+        when(criteriaTransformer.transformCriteria(any())).thenReturn("+firstHighlightFrom:word +secondHighlightFrom:word +keyword:word");
+        
+        // execute
+        SolrQuery solrQuery = queryFactory.createQuery(new JudgmentCriteria(), null);
+        
+        // assert
+        assertEquals("+firstContent:word +firstContent:word +keyword:word", solrQuery.get(HighlightParams.Q));
+    }
+    
     
     //------------------------ PRIVATE --------------------------
     
@@ -94,6 +124,8 @@ public class SearchQueryFactoryImplTest {
         highlightParams.addParam(HighlightParams.SIMPLE_POST, "</highlight>");
         
         HighlightingFieldParams firstFieldHighlightParams = new HighlightingFieldParams("firstContent");
+        firstFieldHighlightParams.addHighlightFromField("firstHighlightFrom");
+        firstFieldHighlightParams.addHighlightFromField("secondHighlightFrom");
         firstFieldHighlightParams.addParam(HighlightParams.FRAGSIZE, "50");
         firstFieldHighlightParams.addParam(HighlightParams.SNIPPETS, "2");
         highlightParams.addFieldParams(firstFieldHighlightParams);
