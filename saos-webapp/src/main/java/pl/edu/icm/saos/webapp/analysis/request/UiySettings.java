80ba4e7cc059d6ff0420c1bfde086ac021d7d20b
Łukasz Dumiszewski <l.dumiszewski@icm.edu.pl>
2015-03-27 07:46:52 +0100

    UiySettings.UiyValueType + converter

--- saos-webapp/src/main/java/pl/edu/icm/saos/webapp/analysis/request/UiySettings.java
+++ saos-webapp/src/main/java/pl/edu/icm/saos/webapp/analysis/request/UiySettings.java
@@ -9,4 +9,41 @@ package pl.edu.icm.saos.webapp.analysis.request;
 
 public class UiySettings {
 
+    
+    
+    public enum UiyValueType {
+        
+        /** Absolute number of results */
+        ABSOLUTE_NUMBER,
+        
+        /** Percent of all possible items */
+        PERCENT,
+        
+        /** Number of results per 1000 items */
+        NUMBER_PER_1000
+        
+    }
+    
+    
+    private UiyValueType valueType = UiyValueType.ABSOLUTE_NUMBER;
+
+    
+    
+    //------------------------ GETTERS --------------------------
+
+    public UiyValueType getValueType() {
+        return valueType;
+    }
+
+    
+    //------------------------ SETTERS --------------------------
+
+    public void setValueType(UiyValueType valueType) {
+        this.valueType = valueType;
+    }
+    
+    
+    
+    
+    
 }
