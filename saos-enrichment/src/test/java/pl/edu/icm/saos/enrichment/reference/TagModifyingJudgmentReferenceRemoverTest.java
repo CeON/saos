d0731d076add9efaf4b1d571d063751f9d5ec258
Krzysztof Mądry <madryk@icm.edu.pl>
2015-04-03 12:43:10 +0200

    #642 code review response

--- saos-enrichment/src/test/java/pl/edu/icm/saos/enrichment/reference/TagModifyingJudgmentReferenceRemoverTest.java
+++ saos-enrichment/src/test/java/pl/edu/icm/saos/enrichment/reference/TagModifyingJudgmentReferenceRemoverTest.java
@@ -58,6 +58,9 @@ public class TagModifyingJudgmentReferenceRemoverTest {
         tagModifyingJudgmentReferenceRemover.setJsonStringWriter(jsonStringWriter);
     }
     
+    
+    //------------------------ TESTS --------------------------
+    
     @Test
     public void removeReference() throws IOException {
         
