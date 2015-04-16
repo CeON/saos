bf0d4b65072825e10bb64c1f76c788076826f048
Łukasz Pawełczak <l.pawelczak@icm.edu.pl>
2015-04-16 06:58:18 +0200

    Closes #670. Search page improvments

--- saos-webapp/src/main/webapp/WEB-INF/static/javascript/judgmentSearch.js
+++ saos-webapp/src/main/webapp/WEB-INF/static/javascript/judgmentSearch.js
@@ -3,7 +3,6 @@
  * 
  * @author Łukasz Pawełczak
  */
-
 var jsInitInJudgmentSearch = function() {
 
 	
@@ -31,26 +30,44 @@ var jsInitInJudgmentSearch = function() {
 		""
 	);
     
-	$('#search-form').validate({
-		onkeyup: false,
-		onclick: false,
-		rules: {
-			dateFrom: {
-				dateFormat: true
-			},
-			dateTo: {
-				dateFormat: true
-			}
-		},
-		errorPlacement: function(error, element) {
-		},
-		highlight: function(element, errorClass, validClass) {
-			$(element).parent().parent().addClass("has-error");
-		},
-		unhighlight: function(element, errorClass, validClass) {
-			$(element).parent().parent().removeClass("has-error");
-		}
-	});
+    $('#search-form').validate({
+        onkeyup: false,
+        onclick: false,
+        rules: {
+            dateFrom: {
+                dateFormat: true
+            },
+            dateTo: {
+                dateFormat: true
+            }
+        },
+        errorPlacement: function(error, element) {},
+        highlight: function(element, errorClass, validClass) {
+            var id = "#" + $(element).attr("id") + "-error",
+            $parent = $(element).parent(); 
+            
+            $parent.addClass("has-error");
+            $parent.prev().addClass("has-error");
+            
+            $(id).css({display: "block"});
+            
+            $parent.parent().css({marginTop: "35px"});
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            var id = "#" + $(element).attr("id") + "-error",
+            $parent = $(element).parent(); 
+            
+            $parent.removeClass("has-error");
+            $parent.prev().removeClass("has-error");
+            
+            $(id).css({display: "none"});
+            
+            if ($parent.parent().find(".has-error").length === 0) {
+            $parent.parent().css({marginTop: "0px"});
+            }
+            
+        }
+    });
     
 	/* Format date */
     $("#datepicker_from, #datepicker_to")
--- saos-webapp/src/main/webapp/WEB-INF/view/search/ccJudgment/ccJudgmentSearchForm.jsp
+++ saos-webapp/src/main/webapp/WEB-INF/view/search/ccJudgment/ccJudgmentSearchForm.jsp
@@ -5,7 +5,15 @@
 
 <saosSearch:courtSelect items="${commonCourtDivisions}" selectedItem="${judgmentCriteriaForm.commonCourtDivisionId}" path="commonCourtDivisionId" id="select-common-division" labelName="select-common-division" labelText="judgmentSearch.formField.commonDivision"  labelEmptyOption="judgmentSearch.formField.chooseCcDivision" />
 
-<saos:formFieldText path="keywords" labelName="input-search-keywords-cc" labelText="judgmentSearch.formField.keywords" />
-
-
+<div class="form-group">
+    <label for="input-search-keywords-cc" class="col-lg-2 col-sm-3 col-xs-12 control-label"><spring:message code="judgmentSearch.formField.keywords" />:</label>
+    <div class="col-lg-7 col-sm-8 col-xs-11">
+        <form:input path="keywords" class="form-control" id="input-search-keywords-cc" />
+    </div>
+    <spring:message code="judgmentSearch.hint.ccKeywords.title" var="hintCcKeywordsTitle" />
+    <spring:message code="judgmentSearch.hint.ccKeywords.content" var="hintCcKeywordsContent" />
+    <div >
+        <saos:hint title="${hintCcKeywordsTitle}" content="${hintCcKeywordsContent}" placement="left" />
+    </div>
+</div>
 
