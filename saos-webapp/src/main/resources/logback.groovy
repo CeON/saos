3fa529f66995a58704616d9fc90dc1875abf4d7e
Łukasz Dumiszewski <l.dumiszewski@icm.edu.pl>
2015-05-29 13:20:51 +0200

    Closes #773: Logback errors emails from production and test servers

--- saos-webapp/src/main/resources/logback.groovy
+++ saos-webapp/src/main/resources/logback.groovy
@@ -8,20 +8,47 @@ import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.rolling.RollingFileAppender
 import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
 import ch.qos.logback.core.status.OnConsoleStatusListener
-
+import ch.qos.logback.classic.net.SMTPAppender
+import ch.qos.logback.classic.PatternLayout
+import com.google.common.base.Preconditions
 
 statusListener(OnConsoleStatusListener)
+
+def HOSTNAME = hostname
 
 def defaultPattern = "%d{HH:mm:ss.SSS} %-5level: [%thread] %logger{36} - %msg%n"
 def logDirectory = System.properties.getProperty('catalina.base', System.getProperty('java.io.tmpdir'))+"/logs"
 
 def props = new Properties()
-def propsFile = new File(System.properties['user.home'], '.icm/saos.logback.properties')
-if (propsFile.exists()) {
-  propsFile.withInputStream {
-    props.load(it)
-  }
+
+
+def defaultPropsFile = new File(getClass().getClassLoader().getResource("logback.default.properties").getFile());
+
+def customPropsFile = new File(System.properties['user.home'], '.icm/saos.logback.properties')
+
+
+if (!defaultPropsFile.exists() && !customPropsFile.exists()) {
+    throw new IllegalStateException("no default or custom logback properties file has been found");
 }
+
+if (defaultPropsFile.exists()) {
+    defaultPropsFile.withInputStream {
+        props.load(it)
+    }
+}
+
+if (customPropsFile.exists()) {
+    println "custom logback properies found: " + customPropsFile.getAbsolutePath()
+    customPropsFile.withInputStream {
+        props.load(it)
+    }
+}
+    
+println "====Logger properties==="    
+for (e in props) {
+    println e.key + ":" + e.value 
+}
+
 
 addInfo("log directory: ${logDirectory}")
 
@@ -43,13 +70,49 @@ appender("FILE", RollingFileAppender) {
     }
 }
 
+def mailTo = props."mailAppender.mailTo"
 
-root(toLevel(props.logLevel ?: "INFO"), ["STDOUT", "FILE"])
 
-println "===Logger levels==="
+
+if (mailTo) {
+
+
+    def mailFrom = props."mailAppender.from";
+    def mailSmtpHost = props."mailAppender.smtpHost"
+    def mailSubjectPattern = props."mailAppender.subjectPattern"
+    def mailContentPattern = props."mailAppender.contentPattern"
+    
+    Preconditions.checkNotNull(mailFrom);
+    Preconditions.checkNotNull(mailSmtpHost);
+    Preconditions.checkNotNull(mailSubjectPattern);
+    Preconditions.checkNotNull(mailContentPattern);
+    
+    mailFrom = mailFrom.replace("\${HOSTNAME}", "${HOSTNAME}");
+    
+    appender("MAIL", SMTPAppender) {
+        smtpHost = mailSmtpHost
+        to = mailTo
+        from = mailFrom
+        subject = mailSubjectPattern
+        layout(PatternLayout) {
+            pattern = mailContentPattern
+        }
+    }
+
+    root(toLevel(props.logLevel ?: "INFO"), ["STDOUT", "FILE", "MAIL"])
+
+
+} else {
+
+    root(toLevel(props.logLevel ?: "INFO"), ["STDOUT", "FILE"])
+
+}
+
 for (e in props) {
-    println e.key + ":" + e.value
-  if (e.key.startsWith('logger.')) { logger(e.key[7..-1], toLevel(e.value.trim())) }
+  
+  if (e.key.startsWith('logger.')) {
+    logger(e.key[7..-1], toLevel(e.value.trim())) 
+  }
 }
 
 
