872a7c892428f44e34dacac5ae0ca854663ab1b4
Krzysztof Mądry <madryk@icm.edu.pl>
2015-06-22 17:53:59 +0200

    #759. Mocking SimpleEntityConverter in CcListServiceTest and ScListServiceTest

--- saos-webapp/src/test/java/pl/edu/icm/saos/webapp/court/CcListServiceTest.java
+++ saos-webapp/src/test/java/pl/edu/icm/saos/webapp/court/CcListServiceTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -23,104 +24,138 @@ import pl.edu.icm.saos.persistence.repository.CommonCourtRepository;
 @RunWith(MockitoJUnitRunner.class)  
 public class CcListServiceTest {
 
-	private CcListService ccListService = new CcListService();
-	
-	private SimpleEntityConverter simpleEntityConverter = new SimpleEntityConverter();
-	
-	private SimpleCommonCourtConverter simpleCommonCourtConverter = new SimpleCommonCourtConverter();
-	
-	@Mock
-	private CommonCourtRepository commonCourtRepository;
-	
-	@Mock
-	private CcDivisionRepository ccDivisionRepository;
-	
-	
-	//------------------------ TESTS --------------------------
-	
-	@Test
-	public void findCommonCourts_correct_order() {
-		//given
-		CommonCourt commonCourtOne = new CommonCourt();
-		commonCourtOne.setName("Sąd w Aninie");
-		CommonCourt commonCourtTwo = new CommonCourt();
-		commonCourtTwo.setName("Sąd w Łodzi");
-		CommonCourt commonCourtThree = new CommonCourt();
-		commonCourtThree.setName("Sąd w Ryczywołach");
-		
-		List<CommonCourt> courtsWrongOrder = Arrays.asList(commonCourtThree, commonCourtOne, commonCourtTwo);
-		
-		when(commonCourtRepository.findAll()).thenReturn(courtsWrongOrder);
-		ccListService.setCommonCourtRepository(commonCourtRepository);
-		ccListService.setSimpleCommonCourtConverter(simpleCommonCourtConverter);
-		
-		
-		//when
-		List<SimpleCommonCourt> courts = ccListService.findCommonCourts();
-		
-		
-		//then
-		assertEquals(3, courts.size());
-		assertEquals(courts.get(0).getName(), commonCourtOne.getName());
-		assertEquals(courts.get(1).getName(), commonCourtTwo.getName());
-		assertEquals(courts.get(2).getName(), commonCourtThree.getName());
-	}
-	
-	@Test
-	public void findCcDivisions_correct_order() {
-		//given
-		CommonCourtDivision ccDivisionOne = new CommonCourtDivision();
-		ccDivisionOne.setName("I Wydział Cywilny");
-		CommonCourtDivision ccDivisionTwo = new CommonCourtDivision();
-		ccDivisionTwo.setName("IV Wydział Karny");
-		CommonCourtDivision ccDivisionThree = new CommonCourtDivision();
-		ccDivisionThree.setName("IX Wydział Penitencjarny i Nadzoru nad Wykonywaniem Orzeczeń Karnych");
-		
-		List<CommonCourtDivision> divisionsWrongOrder = Arrays.asList(ccDivisionThree, ccDivisionOne, ccDivisionTwo);
-		
-		when(ccDivisionRepository.findAllByCourtId(1)).thenReturn(divisionsWrongOrder);
-		ccListService.setCcDivisionRepository(ccDivisionRepository);
-		ccListService.setSimpleEntityConverter(simpleEntityConverter);
-		
-		
-		//when
-		List<SimpleEntity> divisions = ccListService.findCcDivisions(1);
-		
-		
-		//then
-		assertEquals(3, divisions.size());
-		assertEquals(divisions.get(0).getName(), ccDivisionOne.getName());
-		assertEquals(divisions.get(1).getName(), ccDivisionTwo.getName());
-		assertEquals(divisions.get(2).getName(), ccDivisionThree.getName());
-	}
-	
-	@Test
-	public void findCcDivisions_correct_order_names_without_roman_numbers() {
-		//given
-		CommonCourtDivision ccDivisionOne = new CommonCourtDivision();
-		ccDivisionOne.setName("Wydział Cywilny");
-		CommonCourtDivision ccDivisionTwo = new CommonCourtDivision();
-		ccDivisionTwo.setName("Wydział Karny");
-		CommonCourtDivision ccDivisionThree = new CommonCourtDivision();
-		ccDivisionThree.setName("IX Wydział Penitencjarny i Nadzoru nad Wykonywaniem Orzeczeń Karnych");
-		
-		List<CommonCourtDivision> divisionsWrongOrder = Arrays.asList(ccDivisionThree, ccDivisionTwo, ccDivisionOne);
-		
-		when(ccDivisionRepository.findAllByCourtId(1)).thenReturn(divisionsWrongOrder);
-		ccListService.setCcDivisionRepository(ccDivisionRepository);
-		ccListService.setSimpleEntityConverter(simpleEntityConverter);
-		
-		
-		//when
-		List<SimpleEntity> divisions = ccListService.findCcDivisions(1);
-		
-		
-		//then
-		assertEquals(3, divisions.size());
-		assertEquals(divisions.get(0).getName(), ccDivisionOne.getName());
-		assertEquals(divisions.get(1).getName(), ccDivisionTwo.getName());
-		assertEquals(divisions.get(2).getName(), ccDivisionThree.getName());
-	}
+    @InjectMocks
+    private CcListService ccListService = new CcListService();
+
+    @Mock
+    private SimpleEntityConverter simpleEntityConverter;
+
+    @Mock
+    private SimpleCommonCourtConverter simpleCommonCourtConverter;
+
+    @Mock
+    private CommonCourtRepository commonCourtRepository;
+
+    @Mock
+    private CcDivisionRepository ccDivisionRepository;
+
+
+    //------------------------ TESTS --------------------------
+
+    @Test
+    public void findCommonCourts_correct_order() {
+        //given
+        CommonCourt commonCourtOne = new CommonCourt();
+        commonCourtOne.setCode("0510");
+        commonCourtOne.setName("Sąd w Aninie");
+        CommonCourt commonCourtTwo = new CommonCourt();
+        commonCourtTwo.setCode("0520");
+        commonCourtTwo.setName("Sąd w Łodzi");
+        CommonCourt commonCourtThree = new CommonCourt();
+        commonCourtThree.setCode("0530");
+        commonCourtThree.setName("Sąd w Ryczywołach");
+
+        SimpleCommonCourt simpleCommonCourtOne = new SimpleCommonCourt();
+        simpleCommonCourtOne.setName("Sąd w Aninie");
+        SimpleCommonCourt simpleCommonCourtTwo = new SimpleCommonCourt();
+        simpleCommonCourtTwo.setName("Sąd w Łodzi");
+        SimpleCommonCourt simpleCommonCourtThree = new SimpleCommonCourt();
+        simpleCommonCourtThree.setName("Sąd w Ryczywołach");
+
+        List<CommonCourt> courtsWrongOrder = Arrays.asList(commonCourtThree, commonCourtOne, commonCourtTwo);
+        List<CommonCourt> courtsRightOrder = Arrays.asList(commonCourtOne, commonCourtTwo, commonCourtThree);
+
+        when(commonCourtRepository.findAll()).thenReturn(courtsWrongOrder);
+        when(simpleCommonCourtConverter.convertCommonCourts(courtsRightOrder))
+                .thenReturn(Arrays.asList(simpleCommonCourtOne, simpleCommonCourtTwo, simpleCommonCourtThree));
+
+
+        //when
+        List<SimpleCommonCourt> courts = ccListService.findCommonCourts();
+
+
+        //then
+        assertEquals(3, courts.size());
+        assertEquals(courts.get(0).getName(), commonCourtOne.getName());
+        assertEquals(courts.get(1).getName(), commonCourtTwo.getName());
+        assertEquals(courts.get(2).getName(), commonCourtThree.getName());
+    }
+
+    @Test
+    public void findCcDivisions_correct_order() {
+        //given
+        CommonCourtDivision ccDivisionOne = new CommonCourtDivision();
+        ccDivisionOne.setCode("0001");
+        ccDivisionOne.setName("I Wydział Cywilny");
+        CommonCourtDivision ccDivisionTwo = new CommonCourtDivision();
+        ccDivisionTwo.setCode("0002");
+        ccDivisionTwo.setName("IV Wydział Karny");
+        CommonCourtDivision ccDivisionThree = new CommonCourtDivision();
+        ccDivisionThree.setCode("0003");
+        ccDivisionThree.setName("IX Wydział Penitencjarny i Nadzoru nad Wykonywaniem Orzeczeń Karnych");
+
+        SimpleEntity simpleCcDivisionOne = new SimpleEntity();
+        simpleCcDivisionOne.setName("I Wydział Cywilny");
+        SimpleEntity simpleCcDivisionTwo = new SimpleEntity();
+        simpleCcDivisionTwo.setName("IV Wydział Karny");
+        SimpleEntity simpleCcDivisionThree = new SimpleEntity();
+        simpleCcDivisionThree.setName("IX Wydział Penitencjarny i Nadzoru nad Wykonywaniem Orzeczeń Karnych");
+        
+
+        List<CommonCourtDivision> divisionsWrongOrder = Arrays.asList(ccDivisionThree, ccDivisionOne, ccDivisionTwo);
+        List<CommonCourtDivision> divisionsRightOrder = Arrays.asList(ccDivisionOne, ccDivisionTwo, ccDivisionThree);
+
+        when(ccDivisionRepository.findAllByCourtId(1)).thenReturn(divisionsWrongOrder);
+        when(simpleEntityConverter.convertCcDivisions(divisionsRightOrder))
+                .thenReturn(Arrays.asList(simpleCcDivisionOne, simpleCcDivisionTwo, simpleCcDivisionThree));
+
+
+        //when
+        List<SimpleEntity> divisions = ccListService.findCcDivisions(1);
+
+
+        //then
+        assertEquals(3, divisions.size());
+        assertEquals(divisions.get(0).getName(), ccDivisionOne.getName());
+        assertEquals(divisions.get(1).getName(), ccDivisionTwo.getName());
+        assertEquals(divisions.get(2).getName(), ccDivisionThree.getName());
+    }
+
+    @Test
+    public void findCcDivisions_correct_order_names_without_roman_numbers() {
+        //given
+        CommonCourtDivision ccDivisionOne = new CommonCourtDivision();
+        ccDivisionOne.setName("Wydział Cywilny");
+        CommonCourtDivision ccDivisionTwo = new CommonCourtDivision();
+        ccDivisionTwo.setName("Wydział Karny");
+        CommonCourtDivision ccDivisionThree = new CommonCourtDivision();
+        ccDivisionThree.setName("IX Wydział Penitencjarny i Nadzoru nad Wykonywaniem Orzeczeń Karnych");
+
+        SimpleEntity simpleCcDivisionOne = new SimpleEntity();
+        simpleCcDivisionOne.setName("Wydział Cywilny");
+        SimpleEntity simpleCcDivisionTwo = new SimpleEntity();
+        simpleCcDivisionTwo.setName("Wydział Karny");
+        SimpleEntity simpleCcDivisionThree = new SimpleEntity();
+        simpleCcDivisionThree.setName("IX Wydział Penitencjarny i Nadzoru nad Wykonywaniem Orzeczeń Karnych");
+        
+        List<CommonCourtDivision> divisionsWrongOrder = Arrays.asList(ccDivisionThree, ccDivisionTwo, ccDivisionOne);
+        List<CommonCourtDivision> divisionsRightOrder = Arrays.asList(ccDivisionOne, ccDivisionTwo, ccDivisionThree);
+
+        when(ccDivisionRepository.findAllByCourtId(1)).thenReturn(divisionsWrongOrder);
+        when(simpleEntityConverter.convertCcDivisions(divisionsRightOrder))
+                .thenReturn(Arrays.asList(simpleCcDivisionOne, simpleCcDivisionTwo, simpleCcDivisionThree));
+
+
+        //when
+        List<SimpleEntity> divisions = ccListService.findCcDivisions(1);
+
+
+        //then
+        assertEquals(3, divisions.size());
+        assertEquals(divisions.get(0).getName(), ccDivisionOne.getName());
+        assertEquals(divisions.get(1).getName(), ccDivisionTwo.getName());
+        assertEquals(divisions.get(2).getName(), ccDivisionThree.getName());
+    }
 
 
 }
--- saos-webapp/src/test/java/pl/edu/icm/saos/webapp/court/ScListServiceTest.java
+++ saos-webapp/src/test/java/pl/edu/icm/saos/webapp/court/ScListServiceTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -25,102 +26,123 @@ import pl.edu.icm.saos.persistence.repository.ScJudgmentFormRepository;
 @RunWith(MockitoJUnitRunner.class) 
 public class ScListServiceTest {
 
-	
-	private ScListService scListService = new ScListService();
-	
-	private SimpleEntityConverter simpleEntityConverter = new SimpleEntityConverter();
-	
-	@Mock
-	private ScChamberRepository scChamberRepository;
-	
-	@Mock
-	private ScChamberDivisionRepository scChamberDivisionRepository;
-	
-	@Mock
-	private ScJudgmentFormRepository scJudgmentFormRepository;
-	
-	
-	//------------------------ TESTS --------------------------
-	
-	@Test
-	public void findScChambers_correct_order() {
-		//when
-		SupremeCourtChamber scChamberOne = new SupremeCourtChamber();
-		scChamberOne.setName("Izba Pracy, Ubezpieczeń Społecznych i Spraw Publicznych");
-		SupremeCourtChamber scChamberTwo = new SupremeCourtChamber();
-		scChamberTwo.setName("Izba Wojskowa");
-		
-		List<SupremeCourtChamber> chambersWrongOrder = Arrays.asList(scChamberTwo, scChamberOne);
-		
-		when(scChamberRepository.findAll()).thenReturn(chambersWrongOrder);
-		scListService.setScChamberRepository(scChamberRepository);
-		scListService.setSimpleEntityConverter(simpleEntityConverter);
-		
-		
-		//when
-		List<SimpleEntity> chambers = scListService.findScChambers();
-		
-		
-		//then
-		assertEquals(2, chambers.size());
-		assertEquals(chambers.get(0).getName(), scChamberOne.getName());
-		assertEquals(chambers.get(1).getName(), scChamberTwo.getName());
-	}
-	
-	@Test
-	public void findScChamberDivisions_correct_order() {
-		//given
-		SupremeCourtChamberDivision sccDivisionOne = new SupremeCourtChamberDivision();
-		sccDivisionOne.setName("Wydział Odwoławczo-Kasacyjny");
-		SupremeCourtChamberDivision sccDivisionTwo = new SupremeCourtChamberDivision();
-		sccDivisionTwo.setName("Wydział IV");
-		SupremeCourtChamberDivision sccDivisionThree = new SupremeCourtChamberDivision();
-		sccDivisionThree.setName("Wydział IX");
-		
-		List<SupremeCourtChamberDivision> divisionsWrongOrder = Arrays.asList(sccDivisionTwo, sccDivisionThree, sccDivisionOne);
-		
-		when(scChamberDivisionRepository.findAllByScChamberId(1)).thenReturn(divisionsWrongOrder);
-		scListService.setScChamberDivisionRepository(scChamberDivisionRepository);
-		scListService.setSimpleEntityConverter(simpleEntityConverter);
-		
-		
-		//when
-		List<SimpleEntity> chamberDivisions = scListService.findScChamberDivisions(1);
-		
-		
-		//then
-		assertEquals(3, chamberDivisions.size());
-		assertEquals(chamberDivisions.get(0).getName(), sccDivisionOne.getName());
-		assertEquals(chamberDivisions.get(1).getName(), sccDivisionTwo.getName());
-		assertEquals(chamberDivisions.get(2).getName(), sccDivisionThree.getName());
-	}
-	
-	@Test
-	public void findScJudgmentForms() {
-		//given
-		SupremeCourtJudgmentForm scJudgmentFormOne = new SupremeCourtJudgmentForm();
-		scJudgmentFormOne.setName("wyrok SN SD");
-		SupremeCourtJudgmentForm scJudgmentFormTwo = new SupremeCourtJudgmentForm();
-		scJudgmentFormTwo.setName("postanowienie siedmiu sędziów SN");
-		SupremeCourtJudgmentForm scJudgmentFormThree = new SupremeCourtJudgmentForm();
-		scJudgmentFormThree.setName("uchwała całej izby SN");
-		
-		List<SupremeCourtJudgmentForm> scJudgmentForms = Arrays.asList(scJudgmentFormOne, scJudgmentFormTwo, scJudgmentFormThree);
-		
-		when(scJudgmentFormRepository.findAll()).thenReturn(scJudgmentForms);
-		scListService.setScJudgmentFormRepository(scJudgmentFormRepository);
-		scListService.setSimpleEntityConverter(simpleEntityConverter);
-		
-		
-		//when
-		List<SimpleEntity> convertedScJudgmentForms = scListService.findScJudgmentForms();
-		
-		
-		//then
-		assertEquals(3, convertedScJudgmentForms.size());
-		assertEquals(convertedScJudgmentForms.get(0).getName(), scJudgmentFormOne.getName());
-		assertEquals(convertedScJudgmentForms.get(1).getName(), scJudgmentFormTwo.getName());
-		assertEquals(convertedScJudgmentForms.get(2).getName(), scJudgmentFormThree.getName());
-	}
+    @InjectMocks
+    private ScListService scListService = new ScListService();
+
+    @Mock
+    private SimpleEntityConverter simpleEntityConverter;
+
+    @Mock
+    private ScChamberRepository scChamberRepository;
+
+    @Mock
+    private ScChamberDivisionRepository scChamberDivisionRepository;
+
+    @Mock
+    private ScJudgmentFormRepository scJudgmentFormRepository;
+
+
+    //------------------------ TESTS --------------------------
+
+    @Test
+    public void findScChambers_correct_order() {
+        //when
+        SupremeCourtChamber scChamberOne = new SupremeCourtChamber();
+        scChamberOne.setName("Izba Pracy, Ubezpieczeń Społecznych i Spraw Publicznych");
+        SupremeCourtChamber scChamberTwo = new SupremeCourtChamber();
+        scChamberTwo.setName("Izba Wojskowa");
+
+        SimpleEntity simpleScChamberOne = new SimpleEntity();
+        simpleScChamberOne.setName("Izba Pracy, Ubezpieczeń Społecznych i Spraw Publicznych");
+        SimpleEntity simpleScChamberTwo = new SimpleEntity();
+        simpleScChamberTwo.setName("Izba Wojskowa");
+
+        List<SupremeCourtChamber> chambersWrongOrder = Arrays.asList(scChamberTwo, scChamberOne);
+        List<SupremeCourtChamber> chambersRightOrder = Arrays.asList(scChamberOne, scChamberTwo);
+
+        when(scChamberRepository.findAll()).thenReturn(chambersWrongOrder);
+        when(simpleEntityConverter.convertScChambers(chambersRightOrder)).thenReturn(Arrays.asList(simpleScChamberOne, simpleScChamberTwo));
+
+
+        //when
+        List<SimpleEntity> chambers = scListService.findScChambers();
+
+
+        //then
+        assertEquals(2, chambers.size());
+        assertEquals(chambers.get(0).getName(), scChamberOne.getName());
+        assertEquals(chambers.get(1).getName(), scChamberTwo.getName());
+    }
+
+    @Test
+    public void findScChamberDivisions_correct_order() {
+        //given
+        SupremeCourtChamberDivision sccDivisionOne = new SupremeCourtChamberDivision();
+        sccDivisionOne.setName("Wydział Odwoławczo-Kasacyjny");
+        SupremeCourtChamberDivision sccDivisionTwo = new SupremeCourtChamberDivision();
+        sccDivisionTwo.setName("Wydział IV");
+        SupremeCourtChamberDivision sccDivisionThree = new SupremeCourtChamberDivision();
+        sccDivisionThree.setName("Wydział IX");
+
+        SimpleEntity simpleSccDivisionOne = new SimpleEntity();
+        simpleSccDivisionOne.setName("Wydział Odwoławczo-Kasacyjny");
+        SimpleEntity simpleSccDivisionTwo = new SimpleEntity();
+        simpleSccDivisionTwo.setName("Wydział IV");
+        SimpleEntity simpleSccDivisionThree = new SimpleEntity();
+        simpleSccDivisionThree.setName("Wydział IX");
+
+        List<SupremeCourtChamberDivision> divisionsWrongOrder = Arrays.asList(sccDivisionTwo, sccDivisionThree, sccDivisionOne);
+        List<SupremeCourtChamberDivision> divisionsRightOrder = Arrays.asList(sccDivisionOne, sccDivisionTwo, sccDivisionThree);
+
+        when(scChamberDivisionRepository.findAllByScChamberId(1)).thenReturn(divisionsWrongOrder);
+        when(simpleEntityConverter.convertScChamberDivisions(divisionsRightOrder))
+                .thenReturn(Arrays.asList(simpleSccDivisionOne, simpleSccDivisionTwo, simpleSccDivisionThree));
+
+
+        //when
+        List<SimpleEntity> chamberDivisions = scListService.findScChamberDivisions(1);
+
+
+        //then
+        assertEquals(3, chamberDivisions.size());
+        assertEquals(chamberDivisions.get(0).getName(), sccDivisionOne.getName());
+        assertEquals(chamberDivisions.get(1).getName(), sccDivisionTwo.getName());
+        assertEquals(chamberDivisions.get(2).getName(), sccDivisionThree.getName());
+    }
+
+    @Test
+    public void findScJudgmentForms() {
+        //given
+        SupremeCourtJudgmentForm scJudgmentFormOne = new SupremeCourtJudgmentForm();
+        scJudgmentFormOne.setName("wyrok SN SD");
+        SupremeCourtJudgmentForm scJudgmentFormTwo = new SupremeCourtJudgmentForm();
+        scJudgmentFormTwo.setName("postanowienie siedmiu sędziów SN");
+        SupremeCourtJudgmentForm scJudgmentFormThree = new SupremeCourtJudgmentForm();
+        scJudgmentFormThree.setName("uchwała całej izby SN");
+
+        SimpleEntity simpleScJudgmentFormOne = new SimpleEntity();
+        simpleScJudgmentFormOne.setName("wyrok SN SD");
+        SimpleEntity simpleScJudgmentFormTwo = new SimpleEntity();
+        simpleScJudgmentFormTwo.setName("postanowienie siedmiu sędziów SN");
+        SimpleEntity simpleScJudgmentFormThree = new SimpleEntity();
+        simpleScJudgmentFormThree.setName("uchwała całej izby SN");
+
+        List<SupremeCourtJudgmentForm> scJudgmentForms = Arrays.asList(scJudgmentFormOne, scJudgmentFormTwo, scJudgmentFormThree);
+
+        when(scJudgmentFormRepository.findAll()).thenReturn(scJudgmentForms);
+        when(simpleEntityConverter.convertScJudgmentForms(scJudgmentForms))
+                .thenReturn(Arrays.asList(simpleScJudgmentFormOne, simpleScJudgmentFormTwo, simpleScJudgmentFormThree));
+
+
+        //when
+        List<SimpleEntity> convertedScJudgmentForms = scListService.findScJudgmentForms();
+
+
+        //then
+        assertEquals(3, convertedScJudgmentForms.size());
+        assertEquals(convertedScJudgmentForms.get(0).getName(), scJudgmentFormOne.getName());
+        assertEquals(convertedScJudgmentForms.get(1).getName(), scJudgmentFormTwo.getName());
+        assertEquals(convertedScJudgmentForms.get(2).getName(), scJudgmentFormThree.getName());
+    }
 
 }
