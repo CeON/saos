b30e5d86440b3afd607e8f50ffadb29b3b152d72
Krzysztof Mądry <madryk@icm.edu.pl>
2015-06-29 16:59:43 +0200

    #903. corrected focusing when using tab or shift+tag after changing pageSize and sorting

--- saos-webapp/src/main/webapp/WEB-INF/static/javascript/modules/search/searchCriteria.js
+++ saos-webapp/src/main/webapp/WEB-INF/static/javascript/modules/search/searchCriteria.js
@@ -11,27 +11,63 @@ var SearchCriteria = {
 		form: "#search-form",
 		
 		init: function() {
+			SearchCriteria.saveInitialValues();
 			SearchCriteria.assignPageSize();
 			SearchCriteria.assignSort();
+			SearchCriteria.assignKeyPress();
 			SearchCriteria.assignTrackingFocus();
 		},
 		
+		saveInitialValues: function() {
+			$(SearchCriteria.pageSize).data("initial-value", $(SearchCriteria.pageSize).val());
+			$(SearchCriteria.sorting).data("initial-value", $(SearchCriteria.sorting).val());
+		},
+		
+
+		
 		/* Page size change automatically submits search form. */
 		assignPageSize: function() {
-			$(SearchCriteria.pageSize).change(function() {
+			$(SearchCriteria.pageSize).change(function(event) {
+				
 				SearchCriteria.submitForm();
 			});
 		},
 		
 		/* Sorting method change automatically submits search form with new criteria. */
 		assignSort: function() {
-			$(SearchCriteria.sorting + ", " + SearchCriteria.direction).change(function() {
+			$(SearchCriteria.sorting + ", " + SearchCriteria.direction).change(function(event) {
 				var $sort = $(SearchCriteria.sorting),
 					value = $sort.find(":selected").attr("value").split(",")[0],
 					checked = $(SearchCriteria.direction + ":checked").length;
 				
 				$sort.find(":selected").attr("value", value + "," + (checked > 0 ? "asc" : "desc"));
+				
 				SearchCriteria.submitForm();
+			});
+		},
+		
+		/* Assign event for detecting tab pressed when criteria changed (in that situation it should lock focus change) */
+		assignKeyPress: function() {
+			$(SearchCriteria.sorting + ", " + SearchCriteria.pageSize).keydown(function(event) {
+				
+				if (event.keyCode == 9) { // Tab
+					
+					if ($(this).data("initial-value") != $(this).val()) {
+						
+						if (event.shiftKey) {
+							shouldFocusOn = $(":tabbable").eq($(":tabbable").index($(this)) - 1).attr("id");
+						} else {
+							shouldFocusOn = $(":tabbable").eq($(":tabbable").index($(this)) + 1).attr("id");
+						}
+						$(SearchCriteria.form).data("focusOn", shouldFocusOn);
+						
+						
+						event.preventDefault();
+						$(this).change();
+					}
+					
+					
+				}
 			});
 		},
 		
